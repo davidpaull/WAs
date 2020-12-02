31ab6d27cf8627faeb9e812dc6e20269aaee8033
David Paull <22582688+davidpaull@users.noreply.github.com>
2020-12-02 16:38:31 -0500

    Update interrupt.lua

--- interrupt.lua
+++ interrupt.lua
@@ -1,5 +1,6 @@
 -- Event type: COMBAT_LOG_EVENT_UNFILTERED
 function()
+    local debugg = false
     local timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = CombatLogGetCurrentEventInfo()
     
     if event == "SPELL_INTERRUPT" then
@@ -8,14 +9,19 @@ function()
         if sourceGUID == UnitGUID("player") then
             local spellId, spellName, spellSchool, eSpellName  = select(12, CombatLogGetCurrentEventInfo())
             
+            -- Return if player is the target also, ie. Quaking
+            if destGUID == UnitGUID("player") then return end
+            
             if destName:sub(-1):find('[sxzSXZ]') then
                 destName = destName .. "'"
             else
                 destName = destName .. "'s"
             end
             
-            -- DEBUG
-            print(sourceGUID, destGUID)
+            if debugg == true then
+                print(sourceGUID, destGUID)
+            end
+            
             
             if in_instance == false then
                 print("Interrupted " .. destName .. " " .. GetSpellLink(eSpellName))
