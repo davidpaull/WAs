c64b1600988d2a48a03344924f4b4919472c07f4
David Paull <22582688+davidpaull@users.noreply.github.com>
2020-12-02 16:38:07 -0500

    Update tranq.lua

--- tranq.lua
+++ tranq.lua
@@ -1,12 +1,14 @@
 -- Event Type: COMBAT_LOG_EVENT_UNFILTERED
 function()
+    local debugg = false
     local timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = CombatLogGetCurrentEventInfo()
     
     if event == "SPELL_DISPEL" then
         local in_instance, instance_type = IsInInstance()
         
         if sourceGUID == UnitGUID("player") then
-            -- Fix issue where players dispels their own debuff (ie. Displacement dispelling movement impairing effects)
+            -- Return if player dispels their own debuff
+            -- ie. Displacement dispels own movement impairing aura
             if destGUID == UnitGUID("player") then return end
             
             local spellId, spellName, spellSchool, amount = select(12, CombatLogGetCurrentEventInfo())
@@ -16,7 +18,11 @@ function()
             else
                 destName = destName .. "'s"
             end
-
+            
+            if debugg == true then
+                print(sourceGUID, destGUID)
+            end
+            
             -- If not in instance, just print - due to restriction to automated messages in /say channel
             if in_instance == false then
                 print("Dispelled " .. destName .. " " .. GetSpellLink(amount))
